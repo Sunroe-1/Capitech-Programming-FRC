5e976c938c7130092b81be22ec761d57a066712f
Capitech10356-1 <jorgehenriquealda23@gmail.com>
2026-02-12 18:22:05 -0300

    Autonomous

--- Robot.java
+++ Robot.java
@@ -1,181 +1,126 @@
+/*===============================IMPORTS===============================*/
+//Pacote que lista os códigos exclusivamente para robôs da FRC
 package frc.robot;
 
-// === IMPORTAÇÕES ===
+//Importações da biblioteca da WPIlib
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 
+//Importações da Rev para utilização dos SparkMax
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+//Import que permite a utilizaçao da unidade de medida em ''Newtons''
+import static edu.wpi.first.units.Units.Newton;
+
+//import da Rev para utiização do encoder
 import com.revrobotics.RelativeEncoder;
 
+//import da biblioteca utilizada para o sensor NAVx2
 import com.studica.frc.AHRS;
 
-public class Robot extends TimedRobot {
-
-    // === AUTONOMOUS CHOOSER ===
-    private static final String kDefaultAuto = "Default";
-    private static final String kCustomAuto = "My Auto";
-    private String m_autoSelected;
-    private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-    // === DRIVE MOTORS ===
-    private final SparkMax RightNEO1 = new SparkMax(1, MotorType.kBrushed);
-    private final SparkMax LeftNEO1  = new SparkMax(4, MotorType.kBrushed);
-
-    // === PID GARRA ===
-    private double armTarget = 0;
-    private double armLastError = 0;
-
-    private final double kP_arm = 0.6;
-    private final double kD_arm = 0.05;
-
-    // === SHOOTER ===
-    private final SparkMax shooter1 = new SparkMax(7, MotorType.kBrushed);
-    private final SparkMaxConfig shooterConfig = new SparkMaxConfig();
-    private double shooterPower = -0.9;
-
-    // ===INDEXTER ===
-    private final SparkMax Indexter = new SparkMax(2, MotorType.kBrushed );
-
-    // === NAVx2 9-EIXOS ===
-    private AHRS navx;
-    private double lastYawError = 0; 
-
-    // === OUTROS ===
-    private final SparkMaxConfig driveConfig = new SparkMaxConfig();
-    private final Timer tempoTimer = new Timer();
-    private final XboxController controller1 = new XboxController(0);
-
-    // ==================== ROBOT INIT ====================
-    @Override
-    public void robotInit() {
-
-    // === CHOOSER ===
-        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-        m_chooser.addOption("My Auto", kCustomAuto);
-        SmartDashboard.putData("Auto choices", m_chooser);
-
-    // === DRIVE CONFIG ===
-        driveConfig.idleMode(IdleMode.kBrake).inverted(false);
-
-        RightNEO1.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        LeftNEO1.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
 
-    // === SHOOTER ===
-        shooterConfig.idleMode(IdleMode.kCoast);
-        shooter1.configure(shooterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-        SmartDashboard.putNumber("Shooter Power", shooterPower);
 
-    // === NAVx2 9-EIXOS ===
-        navx = new AHRS(AHRS.NavXComType.kMXP_SPI);
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot exstudicatends TimedRobot {
+  private Command m_autonomousCommand;
 
-        System.out.println("=== Sistema iniciado ===");
+  private final RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
     }
+  }
 
-    // ==================== AUTONOMOUS ====================
-    @Override
-    public void autonomousInit() {
-        m_autoSelected = m_chooser.getSelected();
-        tempoTimer.reset();
-        tempoTimer.start();
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
+  }
 
-    @Override
-    public void autonomousPeriodic() {}
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-    // ==================== TELEOP ====================
-    @Override
-    public void teleopPeriodic() {
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-        // === JOYSTICK ===
-        double eixoY = -applyDeadband(controller1.getLeftY(), 0.1);
-        double eixoX = applyDeadband(controller1.getLeftX(), 0.1);
-        double speedMultiplier = controller1.getLeftBumper() ? 1.0 : 0.5;
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 
-        // === ARCADE DRIVE ===
-        double leftSpeed  = clamp((eixoY + eixoX) * speedMultiplier, -0.7, 0.7);
-        double rightSpeed = clamp((eixoY - eixoX) * speedMultiplier, -0.7, 0.7);
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
 
-        // === NAVx2 9-EIXOS/PID ===
-        double turnPower = 0;
-        double yaw = navx.getYaw();
-        double kP_turn = 0.02;
-        double kD_turn = 0.003;
-        
-        if (Math.abs(eixoX) < 0.05) {
-        
-            double yawError = -yaw;
-            double yawDerivative = yawError - lastYawError;
-        
-            //turnPower = (kP_turn * yawError) + (kD_turn * yawDerivative);
-            //turnPower = clamp(turnPower, -0.4, 0.4);
-
-            turnPower = 0;
-        
-            lastYawError = yawError;
-        
-        }
-        
-        LeftNEO1.set(leftSpeed + turnPower);
-        RightNEO1.set(rightSpeed - turnPower);
-
-        // === SHOOTER/INTAKE/OUTTAKE ===
-        double shooterCmd = 0;
-
-        if (controller1.getRightTriggerAxis() > 0.1) {
-           shooterCmd = shooterPower;
-              }
-
-        else if (controller1.getBButton()) {
-             shooterCmd = -0.45;
-              }
-
-        else if (controller1.getXButton()) {
-             shooterCmd = 0.45;
-              }
-
-        shooter1.set(shooterCmd);
-      
-        // === INDEXTER ===
-        if (controller1.getBButton()) {
-            Indexter.set(-0.9);
-        } else if (controller1.getXButton()) {
-            Indexter.set(0.43);
-        } else {
-            Indexter.set(0);
-        }
-    }
-
-  
-    @Override
-    public void teleopInit() {
-        navx.zeroYaw();
-        lastYawError = 0;
-    }
-
-    // ==================== UTIL ====================
-    private double applyDeadband(double val, double lim) {
-        return (Math.abs(val) < lim ? 0 : val);
-    }
-
-    private double clamp(double v, double min, double max) {
-        return Math.max(min, Math.min(max, v));
-    }
-
-    // ==================== VAZIO ====================
-    @Override public void disabledInit() {}
-    @Override public void disabledPeriodic() {}
-    @Override public void testInit() {}
-    @Override public void testPeriodic() {}
-    @Override public void simulationInit() {}
-    @Override public void simulationPeriodic() {}
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
